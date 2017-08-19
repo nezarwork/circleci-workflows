381ae14aa6adeddd91d25a43498c8a2d63f13731
Nezar Fadle <nezarfadle@gmail.com>
2017-08-19 23:31:49 +0800

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 echo $CIRCLE_BRANCH
 echo $CI_PULL_REQUESTS
 echo $CI_PULL_REQUEST
-#CIRCLE_PR_BRANCH = `curl -s https://api.github.com/repos/nezarfadle/circleci-workflows/pulls/107 | jq -r '.head.ref' | jq -r '.head.ref'`
+CIRCLE_PR_BRANCH = `curl -s https://api.github.com/repos/nezarfadle/circleci-workflows/$CIRCLE_BRANCH | jq -r '.head.ref' | jq -r '.head.ref'`
+echo $CIRCLE_PR_BRANCH
 
-if [ "$CIRCLE_BRANCH" == "dev" ]
+#if [ "$CIRCLE_BRANCH" == "dev" ]
+if [ "$CIRCLE_PR_BRANCH" == "dev" ]
 then
 	echo "Start tests - Dev branch"
 fi
