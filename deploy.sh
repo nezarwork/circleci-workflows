bc9cc7d583ac3140a57eabca28c5fa4d5e4bad41
Nezar Fadle <nezarfadle@gmail.com>
2017-08-19 23:26:18 +0800

    update circle ci config file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo $CIRCLE_BRANCH
+echo $CI_PULL_REQUESTS
+echo $CI_PULL_REQUEST
+#CIRCLE_PR_BRANCH = `curl -s https://api.github.com/repos/nezarfadle/circleci-workflows/pulls/107 | jq -r '.head.ref' | jq -r '.head.ref'`
+
 if [ "$CIRCLE_BRANCH" == "dev" ]
 then
 	echo "Start tests - Dev branch"
